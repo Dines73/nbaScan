7a740c355a411aaffe090e0ae463b519244b093f
edin <edo.truman@gmail.com>
2021-02-22 20:30:00 +0100

    create tables

--- schema.sql
+++ schema.sql
@@ -10,11 +10,10 @@ CREATE TABLE players (
 
 CREATE TABLE stats (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  opponent VARCHAR(255) NOT NULL,
-  birth_date DATE NOT NULL,
+  opp VARCHAR(255) NOT NULL,
+  bday DATE NOT NULL,
   game_date DATE NOT NULL,
-  pointsPG INT,
+  PpG INT,
   players_id INT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (players_id) REFERENCES players(id)
@@ -22,11 +21,11 @@ CREATE TABLE stats (
 
 CREATE TABLE astro_data (
   id INT NOT NULL AUTO_INCREMENT,
-  sun_position VARCHAR(255) NOT NULL,
-  moon_position VARCHAR(255) NOT NULL,
-  mercury_position VARCHAR(255) NOT NULL,
-  mars_position VARCHAR(255) NOT NULL,
-  jupiter_position VARCHAR(255) NOT NULL,
+  sun VARCHAR(255) NOT NULL,
+  moon VARCHAR(255) NOT NULL,
+  mer VARCHAR(255) NOT NULL,
+  mars VARCHAR(255) NOT NULL,
+  jup VARCHAR(255) NOT NULL,
   stats_id INT NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (stats_id) REFERENCES stats(id)
